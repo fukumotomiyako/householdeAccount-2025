063d65c36758190337cf1fa4879daf8616bc85c4
fukumotomiyako <xfukumoto.miyako@contract.dentsusoken.com>
2025-10-29 17:03:42 +0900

    add backend

---
+++ householdaccount/householdaccount/src/main/java/com/example/householdaccount/form/IncomeForm.java
@@ -0,0 +1,43 @@
+package com.example.householdaccount.form;
+
+import java.util.Date;
+
+public class IncomeForm {
+	private String radioName;
+	private Date date;
+	private String selectIncome;
+	private  Integer price;
+	private String note;
+	
+	public String getRadioName() {
+		return radioName;
+	}
+	public void setRadioName(String radioName) {
+		this.radioName = radioName;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getSelectIncome() {
+		return selectIncome;
+	}
+	public void setSelectIncome(String selectIncome) {
+		this.selectIncome = selectIncome;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	public String getNote() {
+		return note;
+	}
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+}
